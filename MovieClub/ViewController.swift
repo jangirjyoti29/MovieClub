569c587548d2d1c7671ae72346753ac67e6cdc22
jangirjyoti29 <jangirjyoti29>
2021-05-21 11:56:41 +0530

    First commit

---
+++ MovieClub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieClub
+//
+//  Created by Jyoti Suthar on 21/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
